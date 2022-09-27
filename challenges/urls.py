25889a54fd0eb76bc603e48199d556068e563145
Ahmed Iqbal Bhatti <ahmad786687iqbal@gmail.com>
2022-09-28 01:49:15 +0500

    How to create a Dynamic Urls and Views

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
+# Path function is used to define the path or Url of the specific View
+from . import views
 
-from . import  views
+urlpatterns = [
+    path("<month>", views.monthly_challenge),
+    # path("january", views.january),
+    # path("february", views.february),
+    # path("march", views.march),
+    # path("april", views.april),
+    # path("may", views.may),
+    # path("june", views.june),
+    # path("july", views.july),
+    # path("august", views.august),
+    # path("september", views.september),
+    # path("october", views.october),
+    # path("november", views.november),
+    # path("december", views.december),
 
-urlpatterns =[
-    path("january", views.index),
-    path("feb", views.feb)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,11 +1,75 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
 # Create your views here.
+# Http Response is used to set View for the Specific Url
 
-def index(request):
-    return HttpResponse("This works!")
+def monthly_challenge(request, month):
+    challenges_text = 'None'
+    if month == 'january':
+        challenges_text = "january"
+    elif month == 'february':
+        challenges_text = 'february'
+    elif month == 'march':
+        challenges_text = 'march'
+    elif month == 'april':
+        challenges_text = 'april'
+    else:
+        return HttpResponseNotFound("Not Found")
+    return HttpResponse(challenges_text)
 
-def feb(request):
-    return HttpResponse("I am Fabulus")
+# def january(request):
+#     return HttpResponse("<h1>This works!</h1>")
+#
+#
+# def february(request):
+#     return HttpResponse("I am Fabulus")
+#
+#
+# def march(request):
+#     return HttpResponse("I am Master AIB")
+#
+#
+# def april(request):
+#     return HttpResponse("I am Ahmad Iqbal Bhatti")
+#
+#
+# def may(request):
+#     return HttpResponse("I am the Master piece")
+#
+#
+# def june(request):
+#     return HttpResponse("I am not Jealous")
+#
+#
+# def july(request):
+#     return HttpResponse("I love jalabies")
+#
+#
+# def august(request):
+#     return HttpResponse("Its independent day")
+#
+#
+# def september(request):
+#     return HttpResponse("I am Very serious about her")
+#
+#
+# def october(request):
+#     return HttpResponse("I don't like Octopus")
+#
+#
+# def november(request):
+#     return HttpResponse("There is no way to home")
+#
+#
+# def december(request):
+#     return HttpResponse("Haha deliberate")
+#
+#
+#
+#
+#
+
+
+
